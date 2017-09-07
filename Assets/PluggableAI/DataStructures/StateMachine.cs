12dda7ea6f20ec1f7a221192e135e6baa2624bb6
oultrox <jesus-perez95@hotmail.com>
2017-09-07 18:01:40 -0300

    [add] Pluggeable AI WIP / deleted innecesary stuff

---
+++ Assets/PluggableAI/DataStructures/StateMachine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Clase que será usada por las entidades que llamará los estados.
+public class StateMachine : MonoBehaviour {
+
+    public State currentState;
+    public State remainState;
+
+    private bool aiActive;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TransitionToState(State nextState)
+    {
+        if (nextState != remainState)
+        {
+            currentState = nextState;
+        }
+    }
+}
---
+++ Assets/PluggableAI/DataStructures/States/State.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/State")]
+public class State : ScriptableObject
+{
+
+    public Action[] actions;
+    public Transition[] transitions;
+    public Color sceneGizmoColor = Color.grey;
+
+    public void UpdateState(StateMachine controller)
+    {
+        DoAction(controller);
+        CheckTransitions(controller);
+    }
+
+    private void DoAction(StateMachine stateController)
+    {
+        for (int i = 0; i < actions.Length; i++)
+        {
+            actions[i].Act(stateController);
+        }
+    }
+
+    private void CheckTransitions(StateMachine stateController)
+    {
+        for (int i = 0; i < transitions.Length; i++)
+        {
+            bool decisionSucceded = transitions[i].decision.Decide(stateController);
+
+            if (decisionSucceded)
+            {
+                stateController.TransitionToState(transitions[i].trueState);
+            }
+            else
+            {
+                stateController.TransitionToState(transitions[i].falseState);
+            }
+        }
+    }
+}
